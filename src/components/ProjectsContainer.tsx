66a3415ed537ef97b6e71fee66d66bbdc0642c8e
Carlos Leal <carlos.frleal@gmail.com>
2023-10-07 21:12:50 -0300

    layout responsivo tela 768

--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -5,12 +5,11 @@ const ProjectsContainer: React.FC = () => {
     <section id='projects-container'>
       <h2>Projetos</h2>
       <p>
-      No desenvolvimento front-end, utilizo Redux e Context API para gerenciar estados no React,<br />
+      No desenvolvimento front-end, utilizo Redux e Context API para gerenciar estados no React,
       além de explorar ciclos de vida de componentes, hooks e técnicas para evitar prop drilling.
-      <br />
-      Nos projetos back-end, desenvolvo APIs com TypeScript no formato REST usando Express para manipulação de rotas, Sequelize como ORM e MySQL como banco de dados.
-      <br />
-      Adoto uma arquitetura de camadas MSC, focando em testes unitários e de integração para garantir <br /> a qualidade do código.
+      Nos projetos back-end, desenvolvo APIs com TypeScript no formato REST usando Express para manipulação de rotas,
+      Sequelize como ORM e MySQL como banco de dados.
+      Adoto uma arquitetura de camadas MSC, focando em testes unitários e de integração para garantir a qualidade do código.
       </p>
       <a href='https://github.com/carlosleal89' className='projects-btn' target='_blank'>
         Ver Projetos
